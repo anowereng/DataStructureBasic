e69bdd08cd1bba5d02aca9f0458a397b6a9c33ca
anowereng <anowereng@gmail.com>
2021-01-22 12:42:53 +0600

    basic initialize

---
+++ DataStructureBasic/Array_Ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
