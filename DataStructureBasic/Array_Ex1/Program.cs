766244d761d305b60814137e4a9e88548749786c
anowereng <anowereng@gmail.com>
2021-01-23 09:30:08 +0600

    array use duplicate findout functioality add

--- DataStructureBasic/Array_Ex1/Program.cs
+++ DataStructureBasic/Array_Ex1/Program.cs
@@ -6,7 +6,38 @@ namespace Array_Ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" ******** With Array Length ******** ");
+                foreach (var item in WithArrayLength())
+                {
+                    Console.WriteLine(item);
+                }
+            Console.WriteLine(" ******** Without Array Length ******** ");
+                foreach (var item in WithoutArratLength())
+                {
+                    Console.WriteLine(item);
+                }
+            Console.ReadKey();
+        }
+
+        public static int[] WithArrayLength()
+        {
+            int[] numberlist = new int[6];
+            numberlist[0] = 100;
+            numberlist[1] = 200;
+            numberlist[2] = 300;
+            numberlist[3] = 400;
+            numberlist[4] = 500;
+            numberlist[5] = 600;
+            return numberlist;
+        }
+        public static int[] WithoutArratLength()
+        {
+            return  new int[] { 120, 300, 400, 500, 600 };
+        }
+        public static int[] WithoutNewDeclare()
+        {
+            int[] arraylist = { 10, 15, 20 };
+            return arraylist;
         }
     }
 }
---
+++ DataStructureBasic/Array_Ex2_DuplicateRemove/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Array_Ex2_DuplicateRemove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nums = ");
+            string inputitems = Console.ReadLine();
+
+            char[] seperator = new char[] { ',', '[', ']' };
+          
+            string[] inputStringArray = inputitems.Split(seperator);
+
+            int[] inputIntArrays = RemoveDuplicates(inputStringArray);
+            string result = "[";
+            for (int i = 0; i < inputIntArrays.Length; i++)
+            {
+                if (inputIntArrays.Length > 0)
+                {
+                    result += inputIntArrays[i];
+                    if (i != inputIntArrays.Length-1)
+                        result += ",";
+                }
+            }
+            result += "]";
+            Console.WriteLine($"{inputIntArrays.Length}, nums = {result}");
+            Console.ReadKey();
+        }
+
+        public static int[] RemoveDuplicates(string[] numbers)
+        {
+            int[] temparray = { };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int inputnumber = 0;
+                if (int.TryParse(numbers[i], out inputnumber))
+                {
+                    if (temparray.Length != 0)
+                    {
+                        if (!Array.Exists(temparray, z => z == inputnumber))
+                        {
+                            Array.Resize(ref temparray, temparray.Length + 1);
+                            temparray[temparray.Length - 1] = inputnumber;
+                        }
+                    }
+                    else
+                    {
+                        temparray = new int[] { Convert.ToInt32(numbers[i]) };
+                    }
+                }
+            }
+            return temparray;
+        }
+    }
+}
